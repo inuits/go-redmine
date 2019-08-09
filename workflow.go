9e28cb259410bea9c3439df8f9f2ecb9c7ae6ac6
Karel Willems <kwillems@inuits.eu>
2019-08-09 14:16:30 +0200

    add workflow

---
+++ workflow.go
@@ -0,0 +1,54 @@
+package redmine
+
+import (
+	"encoding/json"
+	"errors"
+//	"net/http"
+	"strconv"
+	"strings"
+)
+
+
+type transitionResult struct {
+	 Transition Transition `json:"transition"`
+}
+
+
+type Transition struct {
+	OldStatusID int `json:"old_status_id"`
+    NewStatusID int `json:"new_status_id"`
+}
+
+
+
+
+// Endpoint to get possible transitions for given role and tracker
+
+func (c *Client) Transition (role_id int, tracker_id int) (*Transition, error) {
+	res, err := c.Get(c.endpoint + "/workflow/transitions.xml?" + "role_id=" + strconv.Itoa(role_id) + "&" + "tracker_id=" + strconv.Itoa(tracker_id) + c.apikey)
+
+
+
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	decoder := json.NewDecoder(res.Body)
+	var r transitionResult
+	if res.StatusCode != 200 {
+		var er errorsResult
+		err = decoder.Decode(&er)
+		if err == nil {
+			err = errors.New(strings.Join(er.Errors, "\n"))
+		}
+	} else {
+		err = decoder.Decode(&r)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return &r.Transition, nil
+}
+
+
